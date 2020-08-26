846d28cc12c654e18b68979462d7c88a7874e5e3
Alvyn Abranches <alvynabranches@gmail.com>
2020-08-26 18:42:02 +0530

    26082020v2

--- speech_to_text/requirements.py
+++ speech_to_text/requirements.py
@@ -3,6 +3,6 @@ import sys, subprocess
 try:
     import speech_recognition as _, moviepy as _
 except:
-    subprocess.call(f'{sys.executable} -m pip install speechrecognition moviepy')
+    subprocess.call(f'{sys.executable} -m pip install --upgrade --user speechrecognition moviepy')
 finally:
     subprocess.call(f'{sys.executable} script.py')
